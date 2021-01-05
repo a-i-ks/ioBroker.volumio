bda8f67e3b419cc7ebe292f1c107fe6f11bb3795
André Iske <andre.iske@mailbox.org>
2021-01-05 14:13:25 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'volumio adapter settings': {
+        'en': 'Adapter settings for volumio',
+        'de': 'Adaptereinstellungen für volumio',
+        'ru': 'Настройки адаптера для volumio',
+        'pt': 'Configurações do adaptador para volumio',
+        'nl': 'Adapterinstellingen voor volumio',
+        'fr': "Paramètres d'adaptateur pour volumio",
+        'it': "Impostazioni dell'adattatore per volumio",
+        'es': 'Ajustes del adaptador para volumio',
+        'pl': 'Ustawienia adaptera dla volumio',
+        'zh-cn': 'volumio的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
